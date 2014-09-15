62ed76838d9c43cf4b06850b45e3a5d4aad2ea5b
jasonalmaturner <jasonalmaturner@gmail.com>
2014-09-15 16:54:31 -0600

    finished game and tie. may add alerts later when the game ends

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	//what does this do?
+	//what does this do? Converts the numbers into face cards
 	function convert_value_to_string(value) {
 		if (value > 10) {
 			switch (value) {
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		return value.toString();
 	}
 
-	//what does this do?
+	//what does this do? creating cards as objects with a suit and a number within the array deck
 	var deck = [];
 	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 	for (var i = 0; i<suits.length; i++) {
@@ -28,7 +28,7 @@ $(document).ready(function() {
 		}
 	}
 	
-	//what does this do?
+	//what does this do? shuffles the deck by randomizing
 	var shuffle = function(array) { 
 		var copy = [];
 		var n = array.length; 
@@ -46,18 +46,91 @@ $(document).ready(function() {
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	//divide out the cards into the two arrays
+	var deal = function(array){
+		cards_player_1 = array.slice(0, 26);
+		cards_player_2 = array.slice(26);
+	}
 	
-	
+	deal(shuffle(deck));
+
+	console.log(cards_player_1);
+	console.log(cards_player_2);
+
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	function war() {
+	function war(card1, card2) {
+		if(card1 > card2) {
+			return 1;
+		}
+		else if(card2 > card1) {
+			return 2;
+		}
+		else if(card1 === card2) {
+			return false;
+		}
+	}
+	
+	var tie = function(){
+		var compareTie = war(cards_player_1[3].number, cards_player_2[3].number);
+		if (compareTie === 1) {
+			for(var i = 0; i < 4; i++){
+				cards_player_1.push(cards_player_1[i]);
+				cards_player_1.push(cards_player_2[i]);
+			}
+			cards_player_1.splice(0,4);
+			cards_player_2.splice(0,4);
+		} else if (compareTie === 2) {
+			for(var i = 0; i < 4; i++){
+				cards_player_2.push(cards_player_1[i]);
+				cards_player_1.push(cards_player_2[i]);
+			}
+			cards_player_1.splice(0,4);
+			cards_player_2.splice(0,4);
+		} else if (compareTie === false) {
+			alert('You poopheads. You tied twice in a row. Now you broke the universe.');
+			cards_player_1.push(cards_player_1[0]);
+			cards_player_2.push(cards_player_2[0]);
+			cards_player_1.splice(0,1);
+			cards_player_2.splice(0,1);
+		}
 	}
 	
 	
+	// var showHand = function () {
+	//     var firstHand = [];
+	//     var secondHand = [];
+	//     var displayHand = function (cards, hand) {
+	//         for (var i = 0; i < cards.length; i++) {
+	//             hand[i] = cards[i].number + " of " + cards[i].suit;
+	//         }
+	//     }
+	//     displayHand(cards_player_1, firstHand);
+	//     displayHand(cards_player_2, secondHand);
+	//     console.log("Player One's Hand: " + firstHand)
+	//     console.log("Player Two's Hand: " + secondHand)
+	// }
 	//create a play function
 		//compare the cards
 		//give the winner both cards (at end of deck)
+	// var counter = 0;
 	function play() {
-		
+		// if (counter % 10 === 0){
+		// 	showHand();
+		// }
+		var compare = war(cards_player_1[0].number, cards_player_2[0].number);
+		if(compare === 1){
+			cards_player_1.push(cards_player_1[0], cards_player_2[0]);
+			cards_player_1.splice(0, 1);
+			cards_player_2.splice(0, 1);
+		}
+		else if(compare === 2){
+			cards_player_2.push(cards_player_1[0], cards_player_2[0]);
+			cards_player_1.splice(0, 1);
+			cards_player_2.splice(0, 1);
+		}
+		else if(compare === false){
+			tie();
+		}
+	
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
